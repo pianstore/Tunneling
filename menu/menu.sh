8b9e19a8025d484a9c4c03fc6dec9b2dfa156dae
PIAN STORE <stoksipu@gmail.com>
2025-06-02 03:20:08 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,7 +1,6 @@
  #!/bin/bash
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/pianstore/Tunneling/main/izin> /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -19,7 +18,6 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/pianstore/Tunneling/main/izin| grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -54,7 +52,6 @@ PERMISSION
 if [ "$res" = "Expired" ]; then
 Exp="\e[36mExpired\033[0m"
 else
-Exp=$(curl -sS https://raw.githubusercontent.com/pianstore/Tunneling/main/izin| grep $MYIP | awk '{print $3}')
 fi
 
 # =========================================
